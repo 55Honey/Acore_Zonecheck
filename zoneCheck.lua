472a3cf2e580ab5047aa3dfe20316680e38e7652
55Honey <71938210+55Honey@users.noreply.github.com>
2022-12-18 20:12:38 +0100

    feat: add kicks/teleports for areas

--- zoneCheck.lua
+++ zoneCheck.lua
@@ -21,8 +21,8 @@
 local Config = {}
 
 -- on/off switch (0/1)
-Config.Teleport = 0                     -- Teleports players to home when entering forbidden zone
-Config.Kick     = 1                     -- Kicks players when entering forbidden zone
+Config.TeleportZone = 0                 -- Teleports players to home when entering forbidden zone
+Config.KickZone     = 1                 -- Kicks players when entering forbidden zone
 
 local Config_Zones = {}                 -- forbidden zones
 
@@ -30,17 +30,23 @@ table.insert(Config_Zones, 4080) -- Quel'Danas
 table.insert(Config_Zones, 3537) -- Borean Tundra
 table.insert(Config_Zones, 3711) -- Sholazar Basin
 table.insert(Config_Zones, 4197) -- Wintergrasp
-table.insert(Config_Zones, 210) -- Icecrown
+table.insert(Config_Zones, 210)  -- Icecrown
 table.insert(Config_Zones, 2817) -- Crystalsong Forest
 table.insert(Config_Zones, 4395) -- Dalaran
-table.insert(Config_Zones, 65) -- Dragonblight
-table.insert(Config_Zones, 66) -- Zul'Drak
-table.insert(Config_Zones, 67) -- Storm Peaks
-table.insert(Config_Zones, 394) -- Grizzly Hills
-table.insert(Config_Zones, 495) -- Howling Fjord
+table.insert(Config_Zones, 65)   -- Dragonblight
+table.insert(Config_Zones, 66)   -- Zul'Drak
+table.insert(Config_Zones, 67)   -- Storm Peaks
+table.insert(Config_Zones, 394)  -- Grizzly Hills
+table.insert(Config_Zones, 495)  -- Howling Fjord
 table.insert(Config_Zones, 4742) -- Hrothgar's Landing
-table.insert(Config_Zones, 876) -- GM Island
+table.insert(Config_Zones, 876)  -- GM Island
 
+Config.TeleportArea = 1                -- Teleports players to home when entering forbidden zone
+Config.KickArea     = 0                -- Kicks players when entering forbidden zone
+
+local Config_Areas = {}                -- forbidden areas
+
+table.insert(Config_Areas, 3547) -- Throne of Kil'jaeden
 ------------------------------------------------------------------------------------------------
 -- CONFIG END
 ------------------------------------------------------------------------------------------------
@@ -56,12 +62,24 @@ local function has_value(tab, val)
     return false
 end
 
-local function shouldKick(player)
+local function shouldKickZone(player,newZone)
     if player:GetGMRank() >= 1 then
         return false
     end
-    local zone = player:GetZoneId()
-    if has_value(Config_Zones, zone) then
+
+    if has_value(Config_Zones, newZone) then
+        return true
+    end
+
+    return false
+end
+
+local function shouldKickArea(player,newArea)
+    if player:GetGMRank() >= 1 then
+        return false
+    end
+
+    if has_value(Config_Areas, newArea) then
         return true
     end
 
@@ -78,37 +96,66 @@ local function performTeleport(player)
     player:CastSpell(player, 8690, true) -- 8690 = Hearthstone spell
 end
 
-local function checkPlayerZone(player)
-    if shouldKick(player) then
-        local zone = player:GetZoneId()
-        PrintError("["..FILE_NAME.."] Player " .. player:GetName() .. " entered restricted zone " .. zone .. " (characterId: " .. player:GetGUIDLow() .. ", accountName: " .. player:GetAccountName() .. ", accountId: " .. player:GetAccountId() .. ")")
-        if Config.Teleport == 1 then
+local function checkPlayerZone(player,newZone)
+    if shouldKickZone(player,newZone) then
+        PrintInfo("["..FILE_NAME.."] Player " .. player:GetName() .. " entered restricted zone " .. newZone .. " (characterId: " .. player:GetGUIDLow() .. ", accountName: " .. player:GetAccountName() .. ", accountId: " .. player:GetAccountId() .. ")")
+        if Config.TeleportZone == 1 then
             performTeleport(player)
         end
-        if Config.Kick == 1 then
+        if Config.KickZone == 1 then
+            performKick(player)
+        end
+    end
+end
+
+local function checkPlayerArea(player,newArea)
+    if shouldKickArea(player,newArea) then
+        PrintInfo("["..FILE_NAME.."] Player " .. player:GetName() .. " entered restricted zone " .. newArea .. " (characterId: " .. player:GetGUIDLow() .. ", accountName: " .. player:GetAccountName() .. ", accountId: " .. player:GetAccountId() .. ")")
+        if Config.TeleportArea == 1 then
+            performTeleport(player)
+        end
+        if Config.KickArea == 1 then
             performKick(player)
         end
     end
 end
 
 local function checkZoneLogin(event, player)
-    checkPlayerZone(player)
+    checkPlayerZone(player,player:GetZoneId())
+    checkPlayerArea(player,player:GetAreaId())
 end
 
 local function checkZoneUpdate(event, player, newZone, newArea)
-    checkPlayerZone(player)
+    checkPlayerZone(player,newZone)
+end
+
+local function checkAreaUpdate(event, player, oldArea, newArea)
+    checkPlayerArea(player,newArea)
 end
 
 local PLAYER_EVENT_ON_LOGIN = 3               -- (event, player)
 local PLAYER_EVENT_ON_UPDATE_ZONE = 27        -- (event, player, newZone, newArea)
+local PLAYER_EVENT_ON_UPDATE_AREA = 47        -- (event, player, oldArea, newArea)
 
-RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, checkZoneLogin)
-RegisterPlayerEvent(PLAYER_EVENT_ON_UPDATE_ZONE, checkZoneUpdate)
+RegisterPlayerEvent( PLAYER_EVENT_ON_LOGIN, checkZoneLogin )
+if ( Config.TeleportZone == 1 or Config.KickZone == 1 ) and Config_Zones ~= {} then
+    RegisterPlayerEvent( PLAYER_EVENT_ON_UPDATE_ZONE, checkZoneUpdate )
+end
+
+if ( Config.TeleportArea == 1 or Config.KickArea == 1 ) and Config_Areas ~= {} then
+    RegisterPlayerEvent( PLAYER_EVENT_ON_UPDATE_AREA, checkAreaUpdate )
+end
 
 do
     local zones = ""
-    for _, val in ipairs(Config_Zones) do 
+    for _, val in ipairs(Config_Zones) do
         zones = zones .. val .. ", "
     end
-    PrintInfo("["..FILE_NAME.."] ZoneCheck loaded. Settings: Kick=" .. (Config.Kick==1 and "yes" or "no") .. " Teleport=" .. (Config.Teleport==1 and "yes" or "no") .. " Zones={ " .. zones .. "}")
+    PrintInfo("["..FILE_NAME.."] ZoneCheck loaded. Settings: Kick=" .. (Config.KickZone==1 and "yes" or "no") .. " Teleport=" .. (Config.TeleportZone==1 and "yes" or "no") .. " Zones={ " .. zones .. "}")
+
+    local areas = ""
+    for _, val in ipairs(Config_Areas) do
+        areas = areas .. val .. ", "
+    end
+    PrintInfo("["..FILE_NAME.."] ZoneCheck loaded. Settings: Kick=" .. (Config.KickArea==1 and "yes" or "no") .. " Teleport=" .. (Config.TeleportArea==1 and "yes" or "no") .. " Areas={ " .. areas .. "}")
 end
